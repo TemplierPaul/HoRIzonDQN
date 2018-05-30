1b9f5f922513c11da40add5071569337c3e663df
yiminghan <yiminghan@hotmail.com>
2018-05-30 20:28:51 +0200

    action index in DQN

--- DQN.py
+++ DQN.py
@@ -7,18 +7,18 @@ import pandas as pd
 
 
 # Hyper Parameters
-EPI_FILE = pd.read_csv('test.csv')
-N_ACTIONS = 16
-N_STATES = EPI_FILE.columns.size - 3
+EPI_FILE = pd.read_csv("dataHorizon/out/up_0.csv")
+N_ACTIONS = 10
+N_STATES = EPI_FILE.columns.size - N_ACTIONS - 1
 print("N_ACTIONS:", N_ACTIONS)
-BATCH_SIZE = 10
+BATCH_SIZE = 32
 LR = 0.01                   # learning rate
 EPSILON = 0.9               # greedy policy
 GAMMA = 0.9                 # reward discount
 TARGET_REPLACE_ITER = 10   # target update frequency
-MEMORY_CAPACITY = 100
+MEMORY_CAPACITY = 1000
 N_EPISODE=100
-N_EXP_TOL=200 #
+N_EXP_TOL=200
 
 
 class Net(nn.Module):
@@ -42,16 +42,21 @@ class DQN(object):
 
         self.learn_step_counter = 0                                     # for target updating
         self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))     # initialize memory
+        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 1 + 1))     # initialize memory
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
         self.loss_func = nn.MSELoss()
 
     def choose_action(self, x):
-        action=np.array(EPI_FILE.ix[x, 1])
-        return action
+        action=np.array(EPI_FILE.ix[x, N_STATES :N_STATES+N_ACTIONS])
+        action_index=0
+        for i in range(N_ACTIONS):
+            if(action[i]>0):
+                action_index=i
+                break
+        return action_index
 
     def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, [a, r], s_))
+        transition = np.hstack((s, a, [r], s_))
         # replace the old memory with new memory
         index = self.memory_counter % MEMORY_CAPACITY
         self.memory[index, :] = transition
@@ -68,14 +73,11 @@ class DQN(object):
         b_memory = self.memory[sample_index, :]
         b_s = Variable(torch.FloatTensor(b_memory[:, :N_STATES]))
         b_a = Variable(torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int)))
-        b_r = Variable(torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2]))
+        b_r = Variable(torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+1+1]))
         b_s_ = Variable(torch.FloatTensor(b_memory[:, -N_STATES:]))
-        #print("b_memory=", b_memory)
-        #print("b_a=",b_a)
         # q_eval w.r.t the action in experience
-        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)//value of the chosen action
+        q_eval = self.eval_net(b_s).gather(1, b_a )# shape (batch, 1)//value of the chosen action
         q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate/value of all the actions
-        print("Q_next=",q_next)
         q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
         loss = self.loss_func(q_eval, q_target)
 
@@ -94,28 +96,24 @@ for i_episode in range(N_EPISODE):
         continue
     N_EXP = EPI_FILE.iloc[:, 0].size
     s_i = 1
-    s=np.array(EPI_FILE.ix[s_i, 2:N_STATES+2])
-    # print("s_i=",s_i)
-    # print("s=",EXP_FILE.ix[s_i, 2:N_STATES+2])
+    s=np.array(EPI_FILE.ix[s_i, 0:N_STATES])
     ep_r = 0
     while (s_i<N_EXP-1):
 
-        a = dqn.choose_action(s_i)
+
 
         # take action
-        # print("s_i=", s_i)
-        # print("a=", a)
-        # print("s=", EXP_FILE.ix[s_i, 2:N_STATES + 2])
+        a_index = dqn.choose_action(s_i)
 
-        r = np.array(EPI_FILE.ix[s_i, N_STATES+2])
+        r = np.array(EPI_FILE.ix[s_i, N_STATES+N_ACTIONS])
         s_i=s_i+1
         if s_i>N_EXP_TOL:
             break
-        s_next = np.array(EPI_FILE.ix[s_i, 2:N_STATES+2])
+        s_next = np.array(EPI_FILE.ix[s_i, 0:N_STATES])
         # modify the reward
 
 
-        dqn.store_transition(s, a, r, s_next)
+        dqn.store_transition(s, a_index, r, s_next)
 
         ep_r += r
         if dqn.memory_counter > MEMORY_CAPACITY:
